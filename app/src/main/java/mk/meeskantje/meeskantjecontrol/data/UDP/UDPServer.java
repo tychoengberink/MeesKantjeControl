0477d4e7ed20e3f0844783772940329d2f500593
bblekkink <466872@student.saxion.nl>
2020-04-01 18:22:33 +0200

    UDP

--- app/src/main/java/mk/meeskantje/meeskantjecontrol/data/UDP/UDPServer.java
+++ app/src/main/java/mk/meeskantje/meeskantjecontrol/data/UDP/UDPServer.java
@@ -4,10 +4,13 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class UDPServer extends Thread {
     DatagramSocket socket;
     boolean running;
+    private byte[] buf = null;
+    private ArrayList<byte[]> queue;
 
 
     public void setRunning(boolean running){
@@ -17,6 +20,7 @@ public class UDPServer extends Thread {
     @Override
     public void run() {
 
+        queue = new ArrayList<>();
         running = true;
 
         try {
@@ -24,14 +28,19 @@ public class UDPServer extends Thread {
 
 
             while(running){
-                byte[] buf ="Hello World!".getBytes();
-                DatagramPacket packet = null;
+//                byte[] buf ="Hello World!".getBytes();
+                setBuf();
+                if (this.buf != null) {
+                    DatagramPacket packet = null;
+                    InetAddress addres = InetAddress.getByName("192.168.1.107");
+                    int port = 33333;
+                    packet = new DatagramPacket(this.buf, this.buf.length, addres, port);
 
-                InetAddress addres = InetAddress.getByName("192.168.1.79");
-                int port = 33333;
-                packet = new DatagramPacket(buf, buf.length, addres, port);
+                    socket.send(packet);
+                    this.buf = null;
 
-                socket.send(packet);
+                    //TODO add timer if needed
+                }
                 continue;
 
             }
@@ -42,5 +51,16 @@ public class UDPServer extends Thread {
     }
     public void kill() {
         running = false;
+    }
+
+    public void setBuf() {
+        if (this.queue.size() > 0) {
+            this.buf = this.queue.get(0);
+            this.queue.remove(0);
+        }
+    }
+
+    public void addQueue(byte[] message) {
+        this.queue.add(message);
     }
 }
